d1a42abcf2f54e89f7eb95e278eb89d7d7c4e6ae
Larry Gamboa <techmedia.lg@gmail.com>
2020-10-17 09:31:14 -0700

    password confirm messages

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 // Password Characters Strings
-var numericChar = "1234567890";
-var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
+var passwordCharSet = "";
 var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
+var numericChar = "1234567890";
 var specialChar = "~`@#$%^&*()_+=";
 
 // Create a function to prompt the user to enter length of the new password
@@ -14,7 +15,31 @@ function generatePassword () {
     return generatePassword;
   }
 }
-// Confirms and Prompts for user
+// Confirms for user to enter password characters
+function passwordCharSet () {
+  var confirmUpperCase = confirm ("Would you like to add upper case characters to your password?");
+  var confirmLowerCase = confirm ("Would you like to add lower case characters to your password?");
+  var confirmNumeric = confirm ("Would you like to add numerical characters to your password?");
+  var confirmSpecial = confirm ("Would you like to add special characters to your password?");
+
+  // Validate entries of password characters
+  if (confirmUpperCase === true) {
+    passwordCharSet += upperCaseChar;
+  }
+
+  if (confirmLowerCase === true) {
+    passwordCharSet += lowerCaseChar;
+  }
+
+  if (confirmNumeric === true) {
+    passwordCharSet += numericChar;
+  }
+
+  if (confirmSpecial === true) {
+    passwordCharSet += specialChar;
+  }
+  return passwordCharSet;
+}
 
 
 
