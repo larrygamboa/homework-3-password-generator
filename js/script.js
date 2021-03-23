b4ded9f2db56f84d37567a01e5b13929f6471c59
Larry Gamboa <techmedia.lg@gmail.com>
2021-03-22 20:01:55 -0700

    clean up js code

--- js/script.js
+++ js/script.js
@@ -1,87 +1,85 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+//Global Scope Password
+var password = "";
 
-// Function to prompt the user to enter length of the new password
-function pwLength () {
-  // Ask the user to enter length of password
-  var desiredLength = prompt ("Enter the length of your new password. Your new password must be set between 8 and 128 characters.");
+var generatePassword = function() {
+  // Call in Global Password here to send out at end
+  password = "";
 
-  // Password length validation
-  if (desiredLength < 8 || desiredLength > 128 || desiredLength === "") {
-    alert ("That's an invalid password length. Your new password must be set between 8 and 128 characters.");
-    return pwLength;
-  }
-  
-  // Console log user's entry
-  console.log (desiredLength);
+  //Create a function that will ask user for length of password. Note: Must be between 8 and 128 Chara long
+  var lengthPassword = passwordLength();
 
-  return desiredLength;
-}
+  //Create a function that will determine all inclusions for Character Sets
+  var passwordInclusions = charaInclusions();
+  var passwordInclusionsLength = passwordInclusions.length;
 
-// User to enter password characters
-function enterChar () {
-  // Password Characters Strings
-  var charInPassword = " ";
-  var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
-  var numericChar = "1234567890";
-  var specialChar = "~`@#$%^&*()_+=";
-
-  // User confirms for password
-  var confirmUpperCase = confirm ("Would you like to include uppercase characters in your password?");
-  var confirmLowerCase = confirm ("Would you like to include lowercase characters in your password?");
-  var confirmNumeric = confirm ("Would you like to include numeric characters in your password?");
-  var confirmSpecialChar = confirm ("Would you like to include special characters in your password?");
-
-  if (confirmUpperCase === true ) {
-    charInPassword += upperCaseChar;
-  }
-
-  if (confirmLowerCase === true ) {
-    charInPassword += lowerCaseChar;
-  }
-
-  if (confirmNumeric === true ) {
-    charInPassword += numericChar;
-  }
-
-  if (confirmSpecialChar === true ) {
-    charInPassword += specialChar;
-  }
-
-  // Console log confirmation entries
-  console.log(confirmUpperCase);
-  console.log(confirmLowerCase);
-  console.log(confirmNumeric);
-  console.log(confirmSpecialChar);
-
-  return charInPassword;
-}
-
-// Function to generate the password
-function generatePassword () {
-  // Password string
-  var password = " ";
-
-  // Call back to function for user to enter desired length of the password
-  var lengthofPassword = pwLength ();
-
-  // Call back to function for user to enter password characters
-  var passwordCharacters = enterChar ();
-
-  // Result of generated password
-  for (var i = 0; i < lengthofPassword; i++) {
-    password = passwordCharacters.charAt(Math.floor(Math.random() * lengthofPassword));
+  for (var i = 0; i < lengthPassword; i++) {
+    password += passwordInclusions.charAt(Math.floor(Math.random() * passwordInclusionsLength));
   }
 
   return password;
+};
+
+//This is the function that will prompt for **password length**
+var passwordLength = function() {
+  //First, lets ask the user for the length of the password using a prompt
+  var lengthPass = prompt("Enter Desired Length of New Password. Must Be Between 8 and 128 Characters Long.");
+
+  //Next, lets validate the user input(Whether user entered invalid length or entered nothing at all)
+  if (lengthPass < 8 || lengthPass > 128 || lengthPass === "") {
+    alert("Invalid Length Entry. Length Must Be Between 8 and 128 Characters Long.")
+    return passwordLength;
+  }
+
+  //This turns string into number. Note:refer to documentation if im still confused later.
+  lengthPass = Math.floor(Number(lengthPass));
+
+  //This will send data from this function to layout function!
+  return lengthPass;
 }
+
+//Function That will determine included Character sets
+var charaInclusions = function() {
+  //String to send back inclusions at end
+  var passwordInclusions = "";
+
+  //Character Sets
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "0123456789";
+  var specialChara = "~`!@#$%^&*+<?/;:(=)|{";
+
+  //Ask to include characters(Confirms or Prompts?)
+  var confirmLower = confirm("Include Lower Case Characters?");
+  var confirmUpper = confirm("Include Upper Case Characters");
+  var confirmNumber = confirm("Inlcude Numbers?");
+  var confirmSpecial = confirm("Include Special Characters?");
+
+  //Validate Selections
+  if (confirmLower == true) {
+    passwordInclusions += lowerCase;
+  }
+  if (confirmUpper == true) {
+    passwordInclusions += upperCase;
+  }
+  if (confirmNumber == true) {
+    passwordInclusions += numbers;
+  }
+  if (confirmSpecial == true) {
+    passwordInclusions += specialChara;
+  }
+
+  //Send back results
+  return passwordInclusions;
+}
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
 }
